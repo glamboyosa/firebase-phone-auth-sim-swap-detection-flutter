efe2cc9dbe711fcf2f58313a8d0d554d71b72b09
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2021-09-22 14:59:26 +0100

    feat: return error name

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     if (initializationError) {
-      errorHandler(context, 'Something Went Wrong.', '');
+      errorHandler(context, 'Something Went Wrong.', 'Please restart the app.');
     }
     if(!isInitialized){
        return const Center(
